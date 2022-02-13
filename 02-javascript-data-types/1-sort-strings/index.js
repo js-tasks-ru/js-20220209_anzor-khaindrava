2b094b9ac6b64b909d684b76b98752b19089681b
nambo0diris <anz77or@gmail.com>
2022-02-13 16:13:46 +0300

    finished 3 tasks

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,19 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  let newArr = [];
+  arr.forEach((el)=>{
+    newArr.push(el)
+  })
 
+  newArr.sort(new Intl.Collator(undefined,{localeMatcher:'best fit', sensitivity:'accent'}).compare).sort((a, b)=>{
+    if(a.toLowerCase() === b.toLowerCase()){
+      if(a[0]>b[0]){
+        return 1;
+      }
+      return -1;
+    }
+  })
+  return param === 'asc' ? newArr : newArr.reverse();
 }
+

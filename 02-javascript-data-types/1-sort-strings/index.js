18b2ad8ec540de1e2e6ea37e0591968b0031b40c
nambo0diris <anz77or@gmail.com>
2022-02-17 20:23:40 +0300

    refactoring 1-sort-strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,19 +5,22 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-  let newArr = [];
-  arr.forEach((el)=>{
-    newArr.push(el)
-  })
+  let newArr = [...arr];
+  let options = {
+    localeMatcher:'best fit',
+    sensitivity:'accent',
+    caseFirst:'upper'
+  };
+  if(param === 'asc') {
+    newArr.sort((a, b) => {
+      return a.localeCompare(b,['ru', 'en'],options)
+    })
+  } else {
+    newArr.sort((a, b) => {
+      return b.localeCompare(a,['ru', 'en'],options)
+    })
+  }
 
-  newArr.sort(new Intl.Collator('ru-en',{localeMatcher:'best fit', sensitivity:'accent'}).compare).sort((a, b)=>{
-    if(a.toLowerCase() === b.toLowerCase()){
-      if(a[0]>b[0]){
-        return 1;
-      }
-      return -1;
-    }
-  })
-  return param === 'asc' ? newArr : newArr.reverse();
+  return newArr;
 }
 
